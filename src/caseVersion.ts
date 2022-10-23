9fb4f5ddc3b7302dbd1a67a58e7595fbc5c391c6
Timothy Jones <timothy.l.jones@gmail.com>
2022-10-23 23:56:57 +1100

    fix: Improve logger output

---
+++ src/caseVersion.ts
@@ -0,0 +1,1 @@
+export const caseVersion = '0e71f4-main+0e71f4.SNAPSHOT.Rogue'
--- src/connectors/logger/consoleLogger.ts
+++ src/connectors/logger/consoleLogger.ts
@@ -1,33 +1,51 @@
 /* eslint-disable no-console */
+import { caseVersion } from 'caseVersion';
 import { shouldLog } from 'entities/logger/shouldLog';
 import type { Logger, LogLevel } from 'entities/logger/types';
+import { Console } from 'node:console';
 
 let currentLogLevel: LogLevel = 'info';
+
+const stdoutLogger = new Console({ stdout: process.stdout });
+
+const caseVersionString = `[case@${caseVersion}]`;
 
 export const logger: Logger = {
   info(message: string, ...additional: unknown[]): void {
     if (shouldLog(currentLogLevel, 'info')) {
-      console.log(`${message}`, ...additional);
+      stdoutLogger.log(`${caseVersionString} ${message}`, ...additional);
     }
   },
   warn(message: string, ...additional: unknown[]): void {
     if (shouldLog(currentLogLevel, 'warn')) {
-      console.log(`[WARN]: ${message}`, ...additional);
+      stdoutLogger.log(
+        `${caseVersionString} [WARN]: ${message}`,
+        ...additional
+      );
     }
   },
   error(message: string, ...additional: unknown[]): void {
     if (shouldLog(currentLogLevel, 'error')) {
-      console.log(`[ERROR]: ${message}`, ...additional);
+      stdoutLogger.log(
+        `${caseVersionString} [ERROR]: ${message}`,
+        ...additional
+      );
     }
   },
   debug(message: string, ...additional: unknown[]): void {
     if (shouldLog(currentLogLevel, 'debug')) {
-      console.log(`[DEBUG]: ${message}`, ...additional);
+      stdoutLogger.log(
+        `${caseVersionString} [DEBUG]: ${message}`,
+        ...additional
+      );
     }
   },
   trace(message: string, ...additional: unknown[]): void {
     if (shouldLog(currentLogLevel, 'trace')) {
-      console.log(`[MAINTAINER-DEBUG]: ${message}`, ...additional);
+      stdoutLogger.log(
+        `${caseVersionString} [MAINTAINER-DEBUG]: ${message}`,
+        ...additional
+      );
     }
   },
   setLevel: (newLevel) => {
