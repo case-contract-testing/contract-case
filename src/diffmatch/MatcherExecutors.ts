627522ff39a25e62e41aad595102085b748b6044
Timothy Jones <timothy.l.jones@gmail.com>
2023-01-02 11:30:34 +1100

    refactor: Extract MatcherExecutors to their own file

---
+++ src/diffmatch/MatcherExecutors.ts
@@ -0,0 +1,41 @@
+import type { MatcherExecutor } from 'entities/executors/types';
+import {
+  type AnyCaseNodeType,
+  NUMBER_MATCHER_TYPE,
+  STRING_MATCHER_TYPE,
+  BOOLEAN_MATCHER_TYPE,
+  CASCADING_CONTEXT_MATCHER_TYPE,
+  NULL_MATCHER_TYPE,
+  SHAPED_ARRAY_MATCHER_TYPE,
+  SHAPED_OBJECT_MATCHER_TYPE,
+  HTTP_STATUS_CODE_MATCHER_TYPE,
+  HTTP_RESPONSE_MATCHER_TYPE,
+  HTTP_REQUEST_MATCHER_TYPE,
+  LOOKUP_MATCHER_TYPE,
+} from 'entities/types';
+import { CascadingContext } from './contextShift/CascadingContext';
+import { HttpStatusCodeMatcher } from './leaf/http/HttpStatusCodeMatcher';
+import { BooleanMatcher } from './leaf/primitives/BooleanMatcher';
+import { NullMatcher } from './leaf/primitives/NullMatcher';
+import { NumberMatcher } from './leaf/primitives/NumberMatcher';
+import { StringMatcher } from './leaf/primitives/StringMatcher';
+import { HttpRequestMatcher } from './structure/HttpRequestMatcher';
+import { HttpResponseMatcher } from './structure/HttpResponseMatcher';
+import { LookupMatcher } from './structure/LookupMatcher';
+import { ShapedArrayExecutor } from './structure/ShapedArrayExecutor';
+import { ShapedObjectExecutor } from './structure/ShapedObjectExecutor';
+
+export const MatcherExecutors: { [T in AnyCaseNodeType]: MatcherExecutor<T> } =
+  {
+    [NUMBER_MATCHER_TYPE]: NumberMatcher,
+    [STRING_MATCHER_TYPE]: StringMatcher,
+    [BOOLEAN_MATCHER_TYPE]: BooleanMatcher,
+    [CASCADING_CONTEXT_MATCHER_TYPE]: CascadingContext,
+    [NULL_MATCHER_TYPE]: NullMatcher,
+    [SHAPED_ARRAY_MATCHER_TYPE]: ShapedArrayExecutor,
+    [SHAPED_OBJECT_MATCHER_TYPE]: ShapedObjectExecutor,
+    [HTTP_STATUS_CODE_MATCHER_TYPE]: HttpStatusCodeMatcher,
+    [HTTP_RESPONSE_MATCHER_TYPE]: HttpResponseMatcher,
+    [HTTP_REQUEST_MATCHER_TYPE]: HttpRequestMatcher,
+    [LOOKUP_MATCHER_TYPE]: LookupMatcher,
+  };
--- src/diffmatch/traversals.ts
+++ src/diffmatch/traversals.ts
@@ -2,48 +2,14 @@ import type { MatchContext } from 'entities/context/types';
 import { CaseCoreError } from 'entities/CaseCoreError';
 import { foldIntoContext } from 'entities/context';
 import { inferMatcher } from 'diffmatch/inferMatcher';
-import {
+
+import type { MatcherExecutor } from 'entities/executors/types';
+import type {
   AnyCaseNodeType,
   CaseNodeFor,
   AnyLeafOrStructure,
-  BOOLEAN_MATCHER_TYPE,
-  CASCADING_CONTEXT_MATCHER_TYPE,
-  NULL_MATCHER_TYPE,
-  NUMBER_MATCHER_TYPE,
-  SHAPED_ARRAY_MATCHER_TYPE,
-  SHAPED_OBJECT_MATCHER_TYPE,
-  STRING_MATCHER_TYPE,
-  HTTP_STATUS_CODE_MATCHER_TYPE,
-  HTTP_RESPONSE_MATCHER_TYPE,
-  HTTP_REQUEST_MATCHER_TYPE,
-  LOOKUP_MATCHER_TYPE,
 } from 'entities/types';
-import type { MatcherExecutor } from 'entities/executors/types';
-import { HttpStatusCodeMatcher } from './leaf/http/HttpStatusCodeMatcher';
-import { CascadingContext } from './contextShift/CascadingContext';
-import { BooleanMatcher } from './leaf/primitives/BooleanMatcher';
-import { NullMatcher } from './leaf/primitives/NullMatcher';
-import { NumberMatcher } from './leaf/primitives/NumberMatcher';
-import { StringMatcher } from './leaf/primitives/StringMatcher';
-import { ShapedArrayExecutor } from './structure/ShapedArrayExecutor';
-import { ShapedObjectExecutor } from './structure/ShapedObjectExecutor';
-import { HttpResponseMatcher } from './structure/HttpResponseMatcher';
-import { HttpRequestMatcher } from './structure/HttpRequestMatcher';
-import { LookupMatcher } from './structure/LookupMatcher';
-
-const MatcherExecutors: { [T in AnyCaseNodeType]: MatcherExecutor<T> } = {
-  [NUMBER_MATCHER_TYPE]: NumberMatcher,
-  [STRING_MATCHER_TYPE]: StringMatcher,
-  [BOOLEAN_MATCHER_TYPE]: BooleanMatcher,
-  [CASCADING_CONTEXT_MATCHER_TYPE]: CascadingContext,
-  [NULL_MATCHER_TYPE]: NullMatcher,
-  [SHAPED_ARRAY_MATCHER_TYPE]: ShapedArrayExecutor,
-  [SHAPED_OBJECT_MATCHER_TYPE]: ShapedObjectExecutor,
-  [HTTP_STATUS_CODE_MATCHER_TYPE]: HttpStatusCodeMatcher,
-  [HTTP_RESPONSE_MATCHER_TYPE]: HttpResponseMatcher,
-  [HTTP_REQUEST_MATCHER_TYPE]: HttpRequestMatcher,
-  [LOOKUP_MATCHER_TYPE]: LookupMatcher,
-};
+import { MatcherExecutors } from './MatcherExecutors';
 
 const getExecutor = <T extends AnyCaseNodeType>(
   matcherOrData: CaseNodeFor<T> | AnyLeafOrStructure,
