d8f13af3bf74de667e8f70a8d879752e88c39bc4
Timothy Jones <timothy.l.jones@gmail.com>
2023-03-06 15:57:17 +1100

    ci: Ensure that windows build fails when the build fails

--- ci/build-and-test.sh
+++ ci/build-and-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
+set -eu # We do this twice so that winadows github runners will properly read them
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 # shellcheck source=lib/robust-bash.sh
 . "$SCRIPT_DIR"/lib/robust-bash.sh 
