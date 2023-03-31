ed5f7698b3145910fe9cebfaacc24260ec8d6fbd
Timothy Jones <timothy.l.jones@gmail.com>
2023-03-31 18:43:38 +1100

    test: Add simple tests for some of the failure cases in contractReader

--- src/__tests__/testContext.ts
+++ src/__tests__/testContext.ts
@@ -1,3 +1,35 @@
+import { DataContext, Logger } from '../entities/types';
+
 export const MAINTAINER_TEST_CONTEXT = {
   testRunId: 'MAINTAINER',
+};
+
+const EMPTY_LOGGER: Logger = {
+  error(): void {},
+  warn(): void {},
+  debug(): void {},
+  maintainerDebug(): void {},
+  deepMaintainerDebug(): void {},
+};
+
+export const EMPTY_DATA_CONTEXT: DataContext = {
+  'case:currentRun:context:logLevel': 'none',
+  'case:currentRun:context:location': [],
+  'case:context:matchBy': 'type',
+  'case:context:serialisableTo': 'json',
+  'case:currentRun:context:contractMode': 'write',
+  'case:currentRun:context:printResults': false,
+  'case:currentRun:context:testName': '',
+  'case:currentRun:context:variables': {},
+  'case:currentRun:context:defaultConfig': {},
+  logger: EMPTY_LOGGER,
+  resultPrinter: {
+    printError(): void {},
+    printSuccessTitle(): void {},
+    printFailureTitle(): void {},
+    printDownloadedContract(): void {},
+  },
+  makeLogger(): Logger {
+    return EMPTY_LOGGER;
+  },
 };
---
+++ src/connectors/contractStore/contractReader.spec.ts
@@ -0,0 +1,29 @@
+import { EMPTY_DATA_CONTEXT } from '../../__tests__/testContext';
+import { CaseConfigurationError } from '../../entities';
+import { makeContractStore, readContract } from './contractReader';
+
+describe('readContract', () => {
+  describe("when given a file that doesn't exist", () => {
+    it('fails', () => {
+      expect(() => readContract('this-no-existy.json')).toThrow(
+        CaseConfigurationError
+      );
+    });
+  });
+  describe("when given a file that isn't json", () => {
+    it('fails', () => {
+      expect(() => readContract('./README.md')).toThrow(CaseConfigurationError);
+    });
+  });
+});
+
+describe('contractStore', () => {
+  const store = makeContractStore(EMPTY_DATA_CONTEXT);
+  describe("when given a directory that doesn't exist", () => {
+    it('fails', () => {
+      expect(() => store.readContractsFromDir('this-no-existy-either')).toThrow(
+        CaseConfigurationError
+      );
+    });
+  });
+});
