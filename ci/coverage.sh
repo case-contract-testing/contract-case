3c76632672db658dcfe0762b851f6fe8b03cf6ac
Timothy Jones <timothy.l.jones@gmail.com>
2023-04-18 14:14:51 +1000

    ci: Add separate script for coverage step

---
+++ ci/coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+set -eu # We do this twice so that winadows github runners will properly read them
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
+# shellcheck source=lib/robust-bash.sh
+. "$SCRIPT_DIR"/lib/robust-bash.sh 
+
+npm ci
+npx lerna bootstrap
+
+npx lerna run build
+npx lerna run test
