d5e789cbc1f7d8e5dba8468152bf19bb57a3f1ba
Timothy Jones <timothy.l.jones@gmail.com>
2023-04-27 02:00:29 +1000

    fix: Correct issue where triggers wouldn't get the configuration information

--- packages/case-boundary/src/mappers/triggers.ts
+++ packages/case-boundary/src/mappers/triggers.ts
@@ -7,24 +7,26 @@ import {
 } from '../boundary';
 import { failureToJsError } from './Result';
 
-export const mapTrigger = (trigger: ITriggerFunction) => (): Promise<void> =>
-  trigger.trigger().then((result) => {
-    switch (result._result) {
-      case RESULT_SUCCESS:
-      case RESULT_SUCCESS_HAS_MAP_PAYLOAD:
-        return;
-      case RESULT_FAILURE:
-        throw failureToJsError(result);
-      default:
-        throw new CaseCoreError(
-          `Unknown result type '${result._result}' during trigger handler`
-        );
-    }
-  });
+export const mapTrigger =
+  (trigger: ITriggerFunction) =>
+  (config: Record<string, unknown>): Promise<void> =>
+    trigger.trigger(config).then((result) => {
+      switch (result._result) {
+        case RESULT_SUCCESS:
+        case RESULT_SUCCESS_HAS_MAP_PAYLOAD:
+          return;
+        case RESULT_FAILURE:
+          throw failureToJsError(result);
+        default:
+          throw new CaseCoreError(
+            `Unknown result type '${result._result}' during trigger handler`
+          );
+      }
+    });
 
 export const mapTriggers = (
   triggers: Record<string, ITriggerFunction>
-): Record<string, () => Promise<void>> =>
+): Record<string, (config: Record<string, unknown>) => Promise<void>> =>
   Object.entries(triggers)
     .map(([key, value]) => ({ [`${key}`]: mapTrigger(value) }))
     .reduce(
