bf693d7be0fc0ef6fd3c218d72e420086228f030
Timothy Jones <timothy.l.jones@gmail.com>
2023-04-26 16:48:28 +1000

    fix: Update previously unused mock definitions to match the prefixed format

--- packages/case-entities/src/mocks/mock.types.ts
+++ packages/case-entities/src/mocks/mock.types.ts
@@ -4,11 +4,31 @@ export type AnyMockDescriptorType =
   | typeof MOCK_HTTP_SERVER
   | typeof MOCK_HTTP_CLIENT;
 
-export const MOCK_HTTP_SERVER = 'case:MockHttpServer' as const;
-export const MOCK_HTTP_CLIENT = 'case:MockHttpClient' as const;
+export const MOCK_HTTP_SERVER = '_case:MockHttpServer' as const;
+export const MOCK_HTTP_CLIENT = '_case:MockHttpClient' as const;
+
+export interface InternalContractCaseCoreBehaviour {
+  type: AnyMockDescriptorType;
+
+  stateVariables: 'state' | 'default';
+
+  triggers: 'provided' | 'generated';
+}
+
+export interface InternalContractCaseCoreSetup {
+  /**
+   * Defines how the ContractCase core will behave when writing (ie, defining) an Example of this type.
+   */
+  write: InternalContractCaseCoreBehaviour;
+  /**
+   * Defines how the ContractCase core will behave when reading (ie, verifying) a Example of this type.
+   */
+  read: InternalContractCaseCoreBehaviour;
+}
 
 export interface HasTypeForMockDescriptor<T extends AnyMockDescriptorType> {
-  'case:mock:type': T;
+  '_case:mock:type': T;
+  '_case:run:context:setup': InternalContractCaseCoreSetup;
 }
 
 export const isCaseMock = (
@@ -16,7 +36,7 @@ export const isCaseMock = (
 ): maybeMock is AnyMockDescriptor =>
   typeof maybeMock === 'object' &&
   maybeMock != null &&
-  'case:mock:type' in (maybeMock as AnyMockDescriptor);
+  '_case:mock:type' in (maybeMock as AnyMockDescriptor);
 
 export type AnyMockDescriptor = ConsumeHttpResponse | ProduceHttpResponse;
 
@@ -33,7 +53,7 @@ export interface CoreHttpRequestResponseMatcherPair {
 export interface ConsumeHttpResponse
   extends HasTypeForMockDescriptor<typeof MOCK_HTTP_SERVER>,
     CoreHttpRequestResponseMatcherPair {
-  'case:run:context:setup': {
+  '_case:run:context:setup': {
     write: {
       type: typeof MOCK_HTTP_SERVER;
       stateVariables: 'default';
@@ -50,7 +70,7 @@ export interface ConsumeHttpResponse
 export interface ProduceHttpResponse
   extends HasTypeForMockDescriptor<typeof MOCK_HTTP_CLIENT>,
     CoreHttpRequestResponseMatcherPair {
-  'case:run:context:setup': {
+  '_case:run:context:setup': {
     write: {
       type: typeof MOCK_HTTP_CLIENT;
       stateVariables: 'state';
