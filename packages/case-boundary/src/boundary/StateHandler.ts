1b635a0f3e049e15633cb833c716657b15b7732f
Timothy Jones <timothy.l.jones@gmail.com>
2023-08-21 10:11:42 +0400

    docs: Correct spelling in StateHandler tsdoc

--- packages/case-boundary/src/boundary/StateHandler.ts
+++ packages/case-boundary/src/boundary/StateHandler.ts
@@ -5,7 +5,7 @@ export abstract class BoundaryStateHandler {
   /**
    * Call the user's state setup function
    *
-   * @returns Either a `BouundaryFailure` with
+   * @returns Either a `BoundaryFailure` with
    * `kind=BoundaryFailureKindConstants.CASE_CONFIGURATION_ERROR` or a
    * `BoundarySuccess` when there's no variables returned, or a
    * `BoundarySuccessWithMap` when there are variables returned
@@ -21,7 +21,7 @@ export abstract class BoundaryStateHandlerWithTeardown extends BoundaryStateHand
   /**
    * Call the user's state teardown function
    *
-   * @returns Either a `BouundaryFailure` with `kind=BoundaryFailureKindConstants.CASE_CONFIGURATION_ERROR` or a `BoundarySuccess`
+   * @returns Either a `BoundaryFailure` with `kind=BoundaryFailureKindConstants.CASE_CONFIGURATION_ERROR` or a `BoundarySuccess`
    */
   teardown(): Promise<BoundaryResult> {
     throw new CaseConfigurationError(
