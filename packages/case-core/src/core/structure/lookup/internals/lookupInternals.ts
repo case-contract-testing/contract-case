bc2eac9cb2ebb42044a957ed377501e5e3180c00
Timothy Jones <timothy.l.jones@gmail.com>
2024-01-30 20:55:34 +1100

    fix: Correct an issue where nested lookup matchers would fail the test of whether or not they described the same thing

--- packages/case-core/src/core/structure/lookup/internals/lookupInternals.ts
+++ packages/case-core/src/core/structure/lookup/internals/lookupInternals.ts
@@ -1,8 +1,62 @@
-import { AnyCaseMatcherOrData } from '@contract-case/case-entities-internal';
-import { CaseConfigurationError } from '../../../../entities';
+import {
+  AnyCaseMatcherOrData,
+  isLookupableMatcher,
+} from '@contract-case/case-entities-internal';
+import { CaseConfigurationError, CaseCoreError } from '../../../../entities';
 import type { MatchContextWithoutLookup } from '../../../../entities/types';
 import { rawEquality } from './rawEquals';
 import type { LookupMap, LookupType } from './types';
+
+export const findLookup = (
+  matcherLookup: LookupMap,
+  lookupType: LookupType,
+  uniqueName: string,
+): AnyCaseMatcherOrData | undefined =>
+  matcherLookup[`${lookupType}:${uniqueName}`];
+
+const unboxAllLookups = (
+  matcherLookup: LookupMap,
+  matcherOrData: AnyCaseMatcherOrData,
+): AnyCaseMatcherOrData => {
+  if (
+    matcherOrData === null ||
+    typeof matcherOrData === 'string' ||
+    typeof matcherOrData === 'number' ||
+    typeof matcherOrData === 'boolean'
+  )
+    return matcherOrData;
+  if (
+    typeof matcherOrData === 'function' ||
+    typeof matcherOrData === 'bigint' ||
+    typeof matcherOrData === 'symbol'
+  ) {
+    throw new CaseConfigurationError(
+      `It looks like an object of type '${typeof matcherOrData}' was attempted to be serialised in the contract. This is unsupported`,
+    );
+  }
+
+  if (Array.isArray(matcherOrData)) {
+    return matcherOrData.map((item) => unboxAllLookups(matcherLookup, item));
+  }
+
+  if (isLookupableMatcher(matcherOrData)) {
+    const replacement = findLookup(
+      matcherLookup,
+      `matcher`,
+      matcherOrData['_case:matcher:uniqueName'],
+    );
+    if (replacement === undefined) {
+      throw new CaseCoreError(
+        `The matcher '${matcherOrData['_case:matcher:uniqueName']}' referenced recursively does not appear to exist. This should have been prevented at the time this matcher was saved.`,
+      );
+    }
+    return unboxAllLookups(matcherLookup, replacement);
+  }
+
+  return Object.entries(matcherOrData)
+    .map(([key, value]) => ({ [key]: unboxAllLookups(matcherLookup, value) }))
+    .reduce((acc, curr) => ({ ...acc, ...curr }));
+};
 
 export const addLookup = (
   matcherLookup: LookupMap,
@@ -16,8 +70,14 @@ export const addLookup = (
     `Saving lookup ${lookupType} (${lookupName}):`,
     matcher,
   );
-  if (matcherLookup[lookupName]) {
-    if (!rawEquality(matcher, matcherLookup[lookupName])) {
+  const candidateMatcher = matcherLookup[lookupName];
+  if (candidateMatcher) {
+    if (
+      !rawEquality(
+        unboxAllLookups(matcherLookup, matcher),
+        unboxAllLookups(matcherLookup, candidateMatcher),
+      )
+    ) {
       context.logger.error(
         `The ${lookupType} with the name '${lookupName}' has more than one definition, and they are not the same`,
       );
@@ -42,10 +102,3 @@ export const addLookup = (
     [lookupName]: matcher,
   };
 };
-
-export const findLookup = (
-  matcherLookup: LookupMap,
-  lookupType: LookupType,
-  uniqueName: string,
-): AnyCaseMatcherOrData | undefined =>
-  matcherLookup[`${lookupType}:${uniqueName}`];
