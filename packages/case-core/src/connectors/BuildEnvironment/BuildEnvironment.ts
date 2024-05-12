0078083936e7a51bb2df02dcc7c22541bd7d1b04
Timothy Jones <timothy.l.jones@gmail.com>
2024-05-13 01:07:25 +1000

    fix: Remove current-git-branch, allowing webpack to roll up the package

--- packages/case-core/src/connectors/BuildEnvironment/BuildEnvironment.ts
+++ packages/case-core/src/connectors/BuildEnvironment/BuildEnvironment.ts
@@ -1,13 +1,11 @@
-// I don't know why this error is triggering - I suspect a bug in import/no-extraneous-dependencies
-// eslint-disable-next-line import/no-extraneous-dependencies
 import isCi from 'is-ci';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import branchName from 'current-git-branch';
+import childProcess from 'child_process';
 import { versionFromGitTag } from 'absolute-version';
 import { BuildEnvironment } from '../../core/types';
 
 export const makeEnvironment = (): BuildEnvironment => ({
   isCi: () => isCi,
   version: () => versionFromGitTag(),
-  branch: () => branchName(),
+  branch: () =>
+    childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim(),
 });
