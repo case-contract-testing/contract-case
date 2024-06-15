1ecc0a47c23beab73398ff1d0d2b3a75a0864208
Timothy Jones <timothy.l.jones@gmail.com>
2024-06-15 15:31:10 +1000

    build: Fix install script for connector

--- packages/dsl-java/install-connector.sh
+++ packages/dsl-java/install-connector.sh
@@ -6,5 +6,9 @@ MAIN_REPO_LOCATION=../..
 
 CONNECTOR_LOCATION="$MAIN_REPO_LOCATION/packages/case-connector"
 
+INSTALL_LOCATION=src/main/resources/io/contract_testing/contractcase/client/server
+mkdir -p "$INSTALL_LOCATION"
+
+
 cp "$CONNECTOR_LOCATION/proto/contract_case_stream.proto" src/main/proto
-cp "$CONNECTOR_LOCATION/package/"* src/main/resources/io/contract_testing/contractcase/client/server
+cp "$CONNECTOR_LOCATION/package/"* "$INSTALL_LOCATION"
