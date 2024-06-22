9996d625d77c53b461c23bdfe5e5ca287661a278
Timothy Jones <timothy.l.jones@gmail.com>
2024-06-22 18:25:24 +1000

    chore: Extract proto from new location

--- packages/dsl-java/install-connector.sh
+++ packages/dsl-java/install-connector.sh
@@ -5,10 +5,11 @@ set -eu
 MAIN_REPO_LOCATION=../..
 
 CONNECTOR_LOCATION="$MAIN_REPO_LOCATION/packages/case-connector"
+CONNECTOR_PROTO_LOCATION="$MAIN_REPO_LOCATION/packages/case-connector-proto"
 
 INSTALL_LOCATION=src/main/resources/io/contract_testing/contractcase/client/server
 mkdir -p "$INSTALL_LOCATION"
 
 
-cp "$CONNECTOR_LOCATION/proto/contract_case_stream.proto" src/main/proto
+cp "$CONNECTOR_PROTO_LOCATION/proto/contract_case_stream.proto" src/main/proto
 cp "$CONNECTOR_LOCATION/package/"* "$INSTALL_LOCATION"
