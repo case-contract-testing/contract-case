e90982e6c9445b9543e64b69afc95a384a643947
Timothy Jones <timothy.l.jones@gmail.com>
2024-07-07 15:31:15 +1000

    chore: Add some TS doc to explain the assertable type

--- packages/case-core/src/entities/types.ts
+++ packages/case-core/src/entities/types.ts
@@ -6,10 +6,29 @@ import { MatchResult, SetupInfoFor } from '@contract-case/case-plugin-base';
 
 export * from './states/types';
 
+/**
+ * Assertable holds a reference to the verifiable response of a mock.
+ *
+ * Note that the mock might not yet have been invoked.
+ */
 export type Assertable<
   T extends AnyMockDescriptorType,
   MockSetups = AllMockSetupInfos,
 > = {
+  /**
+   * Contains any information returned after the mock was setup (eg, ports etc)
+   * Used for passing to trigger functions that will invoke the mock.
+   */
   config: SetupInfoFor<MockSetups, T>;
+  /**
+   * Verify the return data in the mock.
+   *
+   * Call this when the mock should have been called, and you're ready
+   * to assert the result. Note that you'll get a failed match if the mock
+   * hasn't actually been invoked.
+   *
+   * @returns A {@link MatchResult} to indicate success / failure of the
+   *    assertion on this mock's expectation.
+   */
   assert: () => Promise<MatchResult>;
 };
