a43929236dcf4130c4ec145bcacc95effacc6f15
Timothy Jones <timothy.l.jones@gmail.com>
2024-10-02 04:34:00 +1000

    fix: Correct broken JS includes

---
+++ packages/case-definition-dsl/script/link-bundled-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eu
+set -eu # We do this twice so that windows github runners will properly read them
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running  
+
+
+BASE=$(realpath "$SCRIPT_DIR"/../../../node_modules/@contract-case/)
+
+find "$BASE"* -exec bash -c 'echo $0; mkdir -p .'"$SCRIPT_DIR"/..'/node_modules/@contract-case/$(basename $0); cp -R "$0/"* ./node_modules/@contract-case/$(basename $0) 2>/dev/null; echo done' '{}' \;
