c17b646d0f52c24032df283c34cc517ff3cb6724
Timothy Jones <timothy.l.jones@gmail.com>
2024-10-02 23:00:25 +1000

    fix: Include pretty-format in package too

--- packages/case-definition-dsl/script/link-bundled-deps.sh
+++ packages/case-definition-dsl/script/link-bundled-deps.sh
@@ -6,3 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the 
 BASE=$(realpath "$SCRIPT_DIR"/../../../node_modules/@contract-case/)
 
 find "$BASE"* -exec bash -c 'echo $0; mkdir -p .'"$SCRIPT_DIR"/..'/node_modules/@contract-case/$(basename $0); cp -R "$0/"* ./node_modules/@contract-case/$(basename $0) 2>/dev/null; echo done' '{}' \;
+
+mkdir -p ./node_modules/pretty-format 
+cp -R "$SCRIPT_DIR"/../../../node_modules/pretty-format/* ./node_modules/pretty-format 
