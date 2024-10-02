352f4b3753c16b83088fb6391aadddd4ef0628d3
Timothy Jones <timothy.l.jones@gmail.com>
2024-10-03 00:04:39 +1000

    fix: Correct included jsii dependencies

--- packages/case-definition-dsl/script/link-bundled-deps.sh
+++ packages/case-definition-dsl/script/link-bundled-deps.sh
@@ -3,9 +3,9 @@ set -eu # We do this twice so that windows github runners will properly read the
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running  
 
 
-BASE=$(realpath "$SCRIPT_DIR"/../../../node_modules/@contract-case/)
+BASE=$(realpath "$SCRIPT_DIR"/../../../node_modules/@contract-case)
 
-find "$BASE"* -exec bash -c 'echo $0; mkdir -p .'"$SCRIPT_DIR"/..'/node_modules/@contract-case/$(basename $0); cp -R "$0/"* ./node_modules/@contract-case/$(basename $0) 2>/dev/null; echo done' '{}' \;
+find "$BASE"/* -exec bash -c 'echo $0; mkdir -p '"$SCRIPT_DIR"/..'/node_modules/@contract-case/$(basename $0); cp -R "$0/"* '"$SCRIPT_DIR"'/../node_modules/@contract-case/$(basename "$0") 2>/dev/null; echo done' '{}' \;
 
-mkdir -p ./node_modules/pretty-format 
-cp -R "$SCRIPT_DIR"/../../../node_modules/pretty-format/* ./node_modules/pretty-format 
+mkdir -p "$SCRIPT_DIR"/../node_modules/pretty-format 
+cp -R "$SCRIPT_DIR"/../../../node_modules/pretty-format/* "$SCRIPT_DIR"/../node_modules/pretty-format 
