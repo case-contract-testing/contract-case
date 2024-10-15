cca80e32f2dd055388e3cadd0239c883d556bb49
Timothy Jones <timothy.l.jones@gmail.com>
2024-10-15 23:12:07 +1100

    fix: Accurately include version numbers at runtime

--- packages/contract-case-cli/src/entities/versionString.ts
+++ packages/contract-case-cli/src/entities/versionString.ts
@@ -1,12 +1,5 @@
-import * as readPackage from 'read-pkg-up';
+import packageJson from '../../package.json' assert { type: 'json' };
 
-const pkg = readPackage.sync({ cwd: __dirname }) || {
-  packageJson: {
-    name: __dirname,
-    version: 'unknown',
-  },
-};
-
-export const versionString = `${pkg.packageJson.name}@${
-  pkg.packageJson.version || 'UNKNOWN-VERSION'
+export const versionString = `${packageJson.name}@${
+  packageJson.version || 'UNKNOWN-VERSION'
 }`.replace('@contract-case/', '');
