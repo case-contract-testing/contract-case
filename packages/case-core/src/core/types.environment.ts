6396925c67719e164ba80924d61e8d31ced74e9b
Timothy Jones <timothy.l.jones@gmail.com>
2024-11-17 21:29:23 +1100

    docs: Update documentation for branch() function

--- packages/case-core/src/core/types.environment.ts
+++ packages/case-core/src/core/types.environment.ts
@@ -1,4 +1,11 @@
 export interface BuildEnvironment {
+  /**
+   * What branch of the git repository is under test in this run
+   *
+   * @returns The branch name, or false if we
+   *     are unable to determine the branch (eg, if
+   *     we're not running in a git repository).
+   */
   branch(): string | false;
   /**
    * Do we think the current environment is running in a CI server?
