7d924d11cafd34e56c2c570f8267b27e97ccc4e8
Timothy Jones <timothy.l.jones@gmail.com>
2025-03-16 02:48:55 +1100

    chore: Run gradle bump on java compile step too

--- packages/dsl-java/install-connector.sh
+++ packages/dsl-java/install-connector.sh
@@ -15,11 +15,3 @@ mkdir -p "$INSTALL_LOCATION"
 cp "$CONNECTOR_PROTO_LOCATION/proto/contract_case_stream.proto" src/main/proto
 cp "$CONNECTOR_LOCATION/package/"* "$INSTALL_LOCATION"
 
-# Update the caseCoreVersion (may be a no-op)
-#
-# The regular expression matches semver:
-# - `[0-9]+\.[0-9]+\.[0-9]+` : The basic major.minor.patch version
-# - `(-[0-9A-Za-z\.-]+)?` : Optional pre-release version
-# - `(\+[0-9A-Za-z\.-]+)?` : Optional build metadata 
-CORE_VERSION_NUMBER=$(jq -r .version "$SCRIPT_DIR/../case-core/package.json")
-sed -E 's/def caseCoreVersion = "([0-9]+\.[0-9]+\.[0-9]+(-[0-9A-Za-z\.-]+)?(\+[0-9A-Za-z\.-]+)?)?"/def caseCoreVersion = "'$CORE_VERSION_NUMBER'"/' "$SCRIPT_DIR/build.gradle" > "$SCRIPT_DIR/build.gradle.tmp" && mv "$SCRIPT_DIR/build.gradle.tmp" "$SCRIPT_DIR/build.gradle"
---
+++ packages/dsl-java/update-build-gradle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
+
+# Update the caseCoreVersion (may be a no-op)
+#
+# The regular expression matches semver:
+# - `[0-9]+\.[0-9]+\.[0-9]+` : The basic major.minor.patch version
+# - `(-[0-9A-Za-z\.-]+)?` : Optional pre-release version
+# - `(\+[0-9A-Za-z\.-]+)?` : Optional build metadata 
+CORE_VERSION_NUMBER=$(jq -r .version "$SCRIPT_DIR/../case-core/package.json")
+sed -E 's/def caseCoreVersion = "([0-9]+\.[0-9]+\.[0-9]+(-[0-9A-Za-z\.-]+)?(\+[0-9A-Za-z\.-]+)?)?"/def caseCoreVersion = "'$CORE_VERSION_NUMBER'"/' "$SCRIPT_DIR/build.gradle" > "$SCRIPT_DIR/build.gradle.tmp" && mv "$SCRIPT_DIR/build.gradle.tmp" "$SCRIPT_DIR/build.gradle"
