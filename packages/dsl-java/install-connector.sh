4e13e85fa888a3a2b53afb195487545d15504e10
Timothy Jones <timothy.l.jones@gmail.com>
2025-03-15 18:50:52 +1100

    chore: Update sed script so it should pass on ubuntu as well

--- packages/dsl-java/install-connector.sh
+++ packages/dsl-java/install-connector.sh
@@ -16,5 +16,10 @@ cp "$CONNECTOR_PROTO_LOCATION/proto/contract_case_stream.proto" src/main/proto
 cp "$CONNECTOR_LOCATION/package/"* "$INSTALL_LOCATION"
 
 # Update the caseCoreVersion (may be a no-op)
+#
+# The regular expression matches semver:
+# - `[0-9]+\.[0-9]+\.[0-9]+` : The basic major.minor.patch version
+# - `(-[0-9A-Za-z\.-]+)?` : Optional pre-release version
+# - `(\+[0-9A-Za-z\.-]+)?` : Optional build metadata 
 CORE_VERSION_NUMBER=$(jq -r .version "$SCRIPT_DIR/../case-core/package.json")
-sed -i '' -E 's/def caseCoreVersion = "([0-9]+\.[0-9]+\.[0-9]+(-[0-9A-Za-z-]+)?(\+[0-9A-Za-z-]+)?)?"/def caseCoreVersion = "'$CORE_VERSION_NUMBER'"/' build.gradle
+sed -E 's/def caseCoreVersion = "([0-9]+\.[0-9]+\.[0-9]+(-[0-9A-Za-z\.-]+)?(\+[0-9A-Za-z\.-]+)?)?"/def caseCoreVersion = "'$CORE_VERSION_NUMBER'"/' "$SCRIPT_DIR/build.gradle" > "$SCRIPT_DIR/build.gradle.tmp" && mv "$SCRIPT_DIR/build.gradle.tmp" "$SCRIPT_DIR/build.gradle"
