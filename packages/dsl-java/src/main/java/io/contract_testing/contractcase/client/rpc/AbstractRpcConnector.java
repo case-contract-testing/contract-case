86a62af6f26292f99f801663e61b40e818f4d156
Timothy Jones <timothy.l.jones@gmail.com>
2025-03-22 16:35:38 +1100

    refactor: Extract ResponseWaiter, making the RPC connector a bit clearer

--- packages/dsl-java/src/main/java/io/contract_testing/contractcase/client/rpc/AbstractRpcConnector.java
+++ packages/dsl-java/src/main/java/io/contract_testing/contractcase/client/rpc/AbstractRpcConnector.java
@@ -29,32 +29,45 @@ import io.grpc.stub.StreamObserver;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.jetbrains.annotations.NotNull;
 
 abstract class AbstractRpcConnector<T extends AbstractMessage, B extends GeneratedMessageV3.Builder<B>> {
 
-  private final ConcurrentMap<String, CompletableFuture<BoundaryResult>> responseFutures = new ConcurrentHashMap<>();
-  private final AtomicInteger nextId = new AtomicInteger();
   private final SendingWorker<T> worker;
 
   private final ManagedChannel channel;
 
-  final CountDownLatch finishLatch = new CountDownLatch(1);
+  /**
+   * Contains any registered functions that can be called back to by the core server.
+   * <p/>
+   * This should probably be moved somewhere other than the connector.
+   */
   private final Map<String, ConnectorInvokableFunction> registeredFunctions = new ConcurrentHashMap<>();
 
+  private final ResponseWaiter responseWaiter = new ResponseWaiter();
+
+  /**
+   * Prevents multiple sends being called at once. Now that there's a full {@link SendingWorker}
+   * thread, this is probably unnecessary
+   */
+  private static final Semaphore sendMutex = new Semaphore(1);
+
+  /**
+   * Used to wait for the underlying workers to stop during shutdown. If this is zero, the workers
+   * have closed, and the connection is ready to be shutdown.
+   */
+  protected final CountDownLatch finishLatch = new CountDownLatch(1);
 
   private static final int DEFAULT_TIMEOUT_SECONDS = 60;
 
-  // Used to indicate that a boundary result has
+  /**
+   * Used to indicate that the overall connection has failed, and no more sending should happen. If
+   * this is set, all further sends will return immediately with this value.
+   */
   private volatile BoundaryResult failedResult;
 
 
@@ -102,12 +115,50 @@ abstract class AbstractRpcConnector<T extends AbstractMessage, B extends Generat
   abstract StreamObserver<T> createConnection(ContractCaseStub asyncStub,
       ContractResponseStreamObserver<T, B> contractResponseStreamObserver);
 
+  /**
+   * Set the message ID on the kind of messages that this connector sends.
+   * <p>
+   * This exists so that we can share implementation between both the Definition and Verification
+   * RPCs without needing to share types.
+   *
+   * @param builder The builder for the core message type of this connector
+   * @param id      The ID to set
+   * @return a built message, with the ID set appropriately
+   */
   abstract T setId(B builder, StringValue id);
 
+  /**
+   * Make a message builder for a response message with the provided response.
+   * <p>
+   * This exists so that we can share implementation between both the Definition and Verification
+   * RPCs without needing to share types.
+   *
+   * @param response The {@link ResultResponse} to attach.
+   * @return A message builder with the provided {@link ResultResponse} already attached
+   */
   abstract B makeResponse(ResultResponse response);
 
+  /**
+   * Make a message builder for an InvokeTest message with the provided invoker ID.
+   * <p>
+   * This exists so that we can share implementation between both the Definition and Verification
+   * RPCs without needing to share types.
+   *
+   * @param invokerId The invoker identifier
+   * @return A message of the invoke test type
+   */
   abstract B makeInvokeTest(StringValue invokerId);
 
+  /**
+   * Make a message builder for an InvokeFunction message with the provided name and arguments.
+   * <p>
+   * This exists so that we can share implementation between both the Definition and Verification
+   * RPCs without needing to share types.
+   *
+   * @param name The name of the function to invoke
+   * @param args A json string of the arguments to pass
+   * @return A message of the invoke test type
+   */
   abstract B makeInvokeFunction(String name, List<String> args);
 
   /**
@@ -143,53 +194,16 @@ abstract class AbstractRpcConnector<T extends AbstractMessage, B extends Generat
    * @return the result returned from the core.
    */
   public ConnectorResult executeCallAndWait(B builder, String reason, int timeoutSeconds) {
-    final var id =
-        "[" + reason + " " + nextId.getAndIncrement() + " " + Thread.currentThread().getName()
-            + "]";
     if (failedResult != null) {
       // We've already failed, so we don't want to send anything
       return ConnectorIncomingMapper.mapBoundaryResult(failedResult);
     }
 
-    var future = new CompletableFuture<BoundaryResult>();
-    responseFutures.put(id, future);
+    var id = responseWaiter.createAwait(reason);
+
     worker.send(setId(builder, ConnectorOutgoingMapper.map(id)), LogLevel.MAINTAINER_DEBUG);
 
-    try {
-      MaintainerLog.log(LogLevel.MAINTAINER_DEBUG, "Waiting for: " + id);
-      return ConnectorIncomingMapper.mapBoundaryResult(future.get(
-          timeoutSeconds * (long) 1000,
-          TimeUnit.SECONDS
-      ));
-    } catch (TimeoutException e) {
-      MaintainerLog.log(LogLevel.MAINTAINER_DEBUG, "Timed out waiting for: " + id);
-      MaintainerLog.log(
-          LogLevel.MAINTAINER_DEBUG,
-          "Remaining futures: " + responseFutures.keySet()
-      );
-
-      return new ConnectorFailure(
-          ConnectorFailureKindConstants.CASE_CORE_ERROR,
-          "Timed out waiting for internal connection to ContractCase for message '" + id + "'",
-          MaintainerLog.CONTRACT_CASE_JAVA_WRAPPER
-      );
-    } catch (ExecutionException e) {
-      MaintainerLog.log(
-          LogLevel.MAINTAINER_DEBUG,
-          "Execution exception waiting for: " + id + "\n" + e
-      );
-      return new ConnectorFailure(
-          ConnectorFailureKindConstants.CASE_CORE_ERROR,
-          "Failed waiting for a response '" + id + "':" + e.getMessage(),
-          MaintainerLog.CONTRACT_CASE_JAVA_WRAPPER
-      );
-    } catch (InterruptedException e) {
-      return new ConnectorFailure(
-          ConnectorFailureKindConstants.CASE_CONFIGURATION_ERROR,
-          "ContractCase was interrupted during its run. This isn't really a configuration error, it usually happens if a user killed the run.",
-          MaintainerLog.CONTRACT_CASE_JAVA_WRAPPER
-      );
-    }
+    return responseWaiter.awaitResponse(id, timeoutSeconds);
   }
 
   /**
@@ -203,28 +217,14 @@ abstract class AbstractRpcConnector<T extends AbstractMessage, B extends Generat
   void cancelAll(ContractCaseCoreError e) {
     var result = ConnectorOutgoingMapper.mapResult(ConnectorExceptionMapper.map(e));
     this.failedResult = result;
-    this.responseFutures.forEach((key, value) -> value.complete(result));
+    responseWaiter.cancelAll(result);
   }
 
-  void completeWait(String id, BoundaryResult result) {
-    MaintainerLog.log(
-        LogLevel.MAINTAINER_DEBUG,
-        "Completing wait for id '" + id + "', with result: " + result.getValueCase().name()
-    );
 
-    final var future = responseFutures.get(id);
-    if (future == null) {
-      throw new ContractCaseCoreError(
-          "There was no future with id '" + id
-              + "'. This is a bug in the wrapper or the boundary.",
-          MaintainerLog.CONTRACT_CASE_JAVA_WRAPPER
-      );
-    }
-    responseFutures.get(id).complete(result);
-    responseFutures.remove(id);
+  void completeWait(String requestId, BoundaryResult result) {
+    responseWaiter.completeAwait(requestId, result);
   }
 
-  private static final Semaphore sendMutex = new Semaphore(1);
 
   void sendResponse(B builder, String id, LogLevel logLevel) {
     try {
@@ -243,6 +243,13 @@ abstract class AbstractRpcConnector<T extends AbstractMessage, B extends Generat
     }
   }
 
+  /**
+   * Sends a response to the core
+   *
+   * @param response the actual response
+   * @param id The message being replied to
+   * @param logLevel
+   */
   void sendResponse(ResultResponse response, String id, LogLevel logLevel) {
     sendResponse(makeResponse(response), id, logLevel);
   }
@@ -262,7 +269,12 @@ abstract class AbstractRpcConnector<T extends AbstractMessage, B extends Generat
     }
   }
 
-
+  /**
+   * Called by the user facing DSL to register functions that the core can call back.
+   *
+   * @param functionName The name (ie, handle) of the function that the Core can use as a callback
+   * @param function     The actual function that can be invoked
+   */
   public <R> void registerFunction(String functionName, ConnectorInvokableFunction function) {
     if (this.registeredFunctions.containsKey(functionName)) {
       throw new ContractCaseConfigurationError("The function '"
@@ -271,7 +283,14 @@ abstract class AbstractRpcConnector<T extends AbstractMessage, B extends Generat
     this.registeredFunctions.put(functionName, function);
   }
 
-  public ConnectorResult invokeFunction(String functionName, List<String> args) {
+  /**
+   * Called by the core to invoke a user-provided function.
+   *
+   * @param functionName The name of the function
+   * @param args         The arguments as strings
+   * @return A {@link ConnectorResult} indicating the result of invoking the function.
+   */
+  ConnectorResult invokeFunction(String functionName, List<String> args) {
     var method = this.registeredFunctions.get(functionName);
     if (method == null) {
       return new ConnectorFailure(
