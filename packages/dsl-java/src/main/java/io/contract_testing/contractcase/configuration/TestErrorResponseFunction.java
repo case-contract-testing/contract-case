4c49cce3c31b9943c58621b9c17b2324baeb73fa
Timothy Jones <timothy.l.jones@gmail.com>
2025-05-26 23:41:07 +1000

    fix(java-dsl): Add `throws Exception` to the signature of the response test methods, so you don't need to re-throw exceptions as `RuntimeException`

--- packages/dsl-java/src/main/java/io/contract_testing/contractcase/configuration/TestErrorResponseFunction.java
+++ packages/dsl-java/src/main/java/io/contract_testing/contractcase/configuration/TestErrorResponseFunction.java
@@ -1,8 +1,17 @@
 package io.contract_testing.contractcase.configuration;
 
-import io.contract_testing.contractcase.configuration.InteractionSetup;
-
 public interface TestErrorResponseFunction {
 
-  void call(Exception e, InteractionSetup config);
+  /**
+   * Called by the framework to test an exception that you expected to throw from your interaction.
+   * <p>
+   * Implement this method with whatever assertions and tests you need to verify that the exception
+   * was the one you expected.
+   *
+   * @param e      The exception thrown by your trigger function.
+   * @param config the interaction setup, useful if you need state variables or other context
+   * @throws Exception Any exceptions thrown during your verification. Throwing an exception from
+   *                   this method will fail the interaction.
+   */
+  void call(Exception e, InteractionSetup config) throws Exception;
 }
--- packages/dsl-java/src/main/java/io/contract_testing/contractcase/configuration/TestResponseFunction.java
+++ packages/dsl-java/src/main/java/io/contract_testing/contractcase/configuration/TestResponseFunction.java
@@ -1,8 +1,18 @@
 package io.contract_testing.contractcase.configuration;
 
-import io.contract_testing.contractcase.configuration.InteractionSetup;
-
 public interface TestResponseFunction<T> {
 
-  void call(T returnedObject, InteractionSetup config);
+  /**
+   * Called by the framework to test the response from your interaction's trigger function..
+   * <p>
+   * Implement this method with whatever assertions and tests you need to verify that you have
+   * unmarshalled the expected object correctly.
+   *
+   * @param returnedObject The object returned from your trigger function.
+   * @param config         the interaction setup, useful if you need state variables or other
+   *                       context
+   * @throws Exception Any exceptions thrown during your verification. Throwing an exception from
+   *                   this method will fail the interaction.
+   */
+  void call(T returnedObject, InteractionSetup config) throws Exception;
 }
