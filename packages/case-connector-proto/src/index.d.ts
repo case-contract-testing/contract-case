a297ce92b4fcfeb3b2c1ed435f27c569e0cdebfc
Timothy Jones <timothy.l.jones@gmail.com>
2025-07-03 23:41:38 +1000

    chore: Fix types for case-core and case-connector-proto

---
+++ packages/case-connector-proto/src/index.d.ts
@@ -0,0 +1,4 @@
+export * from './grpc/proto/contract_case_stream_pb';
+export * from './grpc/proto/contract_case_stream_grpc_pb';
+export type * from './grpc/proto/contract_case_stream_grpc_pb';
+export type * from './grpc/proto/contract_case_stream_pb';
