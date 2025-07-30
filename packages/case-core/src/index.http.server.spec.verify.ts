2cc9974ab8fe5a4cd7210f011126b0245a9a1c60
Timothy Jones <timothy.l.jones@gmail.com>
2025-07-30 21:46:04 +1000

    chore: Fix tests

--- packages/case-core/src/index.http.server.spec.verify.ts
+++ packages/case-core/src/index.http.server.spec.verify.ts
@@ -10,45 +10,10 @@ verifyContract(
     printResults: false,
   },
   (verifier) => {
-    verifier.verifyContract({
-      triggers: {
-        'an http "GET" request to "/health" without a body': {
-          trigger: (setup: HttpRequestConfig) =>
-            api(setup.mock['baseUrl']).health(),
-          testResponses: {
-            'returns a (200) response with body an object shaped like {status: "up"}':
-              (health) => {
-                expect(health).toEqual('up');
-              },
-            'returns a (200) response with body an object shaped like {status: <any string>}':
-              (health) => expect(typeof health).toBe('string'),
-            'a (200) response with body an object shaped like {status: "up"}': (
-              health,
-            ) => {
-              expect(health).toEqual('up');
-            },
-            'a (200) response with body an object shaped like {status: <any string>}':
-              (health) => expect(typeof health).toBe('string'),
-          },
-          testErrorResponses: {
-            'returns a (httpStatus 4XX | 5XX) response without a body': (e) => {
-              expect(e).toBeInstanceOf(ApiError);
-            },
-            'returns a (503) response with body an object shaped like {status: "down"}':
-              (e) => {
-                expect(e).toBeInstanceOf(ApiError);
-              },
-            'a (httpStatus 4XX | 5XX) response without a body': (e) => {
-              expect(e).toBeInstanceOf(ApiError);
-            },
-            'a (503) response with body an object shaped like {status: "down"}':
-              (e) => {
-                expect(e).toBeInstanceOf(ApiError);
-              },
-          },
-        },
-        'an http "GET" request to "/health" with the following headers an object shaped like {accept: "application/json"} without a body':
-          {
+    verifier
+      .prepareVerificationTests({
+        triggers: {
+          'an http "GET" request to "/health" without a body': {
             trigger: (setup: HttpRequestConfig) =>
               api(setup.mock['baseUrl']).health(),
             testResponses: {
@@ -56,81 +21,123 @@ verifyContract(
                 (health) => {
                   expect(health).toEqual('up');
                 },
+              'returns a (200) response with body an object shaped like {status: <any string>}':
+                (health) => expect(typeof health).toBe('string'),
               'a (200) response with body an object shaped like {status: "up"}':
                 (health) => {
                   expect(health).toEqual('up');
                 },
+              'a (200) response with body an object shaped like {status: <any string>}':
+                (health) => expect(typeof health).toBe('string'),
+            },
+            testErrorResponses: {
+              'returns a (httpStatus 4XX | 5XX) response without a body': (
+                e,
+              ) => {
+                expect(e).toBeInstanceOf(ApiError);
+              },
+              'returns a (503) response with body an object shaped like {status: "down"}':
+                (e) => {
+                  expect(e).toBeInstanceOf(ApiError);
+                },
+              'a (httpStatus 4XX | 5XX) response without a body': (e) => {
+                expect(e).toBeInstanceOf(ApiError);
+              },
+              'a (503) response with body an object shaped like {status: "down"}':
+                (e) => {
+                  expect(e).toBeInstanceOf(ApiError);
+                },
             },
           },
-        'an http "GET" request to "/users"?id={{userId}} without a body': {
-          trigger: (setup: HttpRequestConfig) =>
-            api(setup.mock['baseUrl']).getUserByQuery(
-              (setup.stateVariables['userId'] as string) || '123',
-            ),
-          testResponses: {
-            'returns a (200) response with body an object shaped like {userId: {{userId}}}':
-              (user, setup) => {
-                expect(user).toEqual({
-                  userId: setup.stateVariables['userId'],
-                });
+          'an http "GET" request to "/health" with the following headers an object shaped like {accept: "application/json"} without a body':
+            {
+              trigger: (setup: HttpRequestConfig) =>
+                api(setup.mock['baseUrl']).health(),
+              testResponses: {
+                'returns a (200) response with body an object shaped like {status: "up"}':
+                  (health) => {
+                    expect(health).toEqual('up');
+                  },
+                'a (200) response with body an object shaped like {status: "up"}':
+                  (health) => {
+                    expect(health).toEqual('up');
+                  },
               },
-            'a (200) response with body an object shaped like {userId: {{userId}}}':
-              (user, setup) => {
-                expect(user).toEqual({
-                  userId: setup.stateVariables['userId'],
-                });
-              },
-          },
-          testErrorResponses: {
-            'returns a (404) response without a body': (e) => {
-              expect(e).toBeInstanceOf(UserNotFoundConsumerError);
             },
-            'a (404) response without a body': (e) => {
-              expect(e).toBeInstanceOf(UserNotFoundConsumerError);
+          'an http "GET" request to "/users"?id={{userId}} without a body': {
+            trigger: (setup: HttpRequestConfig) =>
+              api(setup.mock['baseUrl']).getUserByQuery(
+                (setup.stateVariables['userId'] as string) || '123',
+              ),
+            testResponses: {
+              'returns a (200) response with body an object shaped like {userId: {{userId}}}':
+                (user, setup) => {
+                  expect(user).toEqual({
+                    userId: setup.stateVariables['userId'],
+                  });
+                },
+              'a (200) response with body an object shaped like {userId: {{userId}}}':
+                (user, setup) => {
+                  expect(user).toEqual({
+                    userId: setup.stateVariables['userId'],
+                  });
+                },
+            },
+            testErrorResponses: {
+              'returns a (404) response without a body': (e) => {
+                expect(e).toBeInstanceOf(UserNotFoundConsumerError);
+              },
+              'a (404) response without a body': (e) => {
+                expect(e).toBeInstanceOf(UserNotFoundConsumerError);
+              },
+            },
+          },
+          'an http "GET" request to "/users/123" without a body': {
+            trigger: (setup: HttpRequestConfig) =>
+              api(setup.mock['baseUrl']).getUserByPath('123'),
+            testErrorResponses: {
+              'returns a (404) response without a body': (e) => {
+                expect(e).toBeInstanceOf(UserNotFoundConsumerError);
+              },
+              'a (404) response without a body': (e) => {
+                expect(e).toBeInstanceOf(UserNotFoundConsumerError);
+              },
+            },
+          },
+          'an http "GET" request to "/users/{{userId}}" without a body': {
+            trigger: (setup: HttpRequestConfig) =>
+              api(setup.mock['baseUrl']).getUserByPath(
+                setup.stateVariables['userId'] as string,
+              ),
+            testResponses: {
+              'returns a (200) response with body an object shaped like {userId: {{userId}}}':
+                (user, setup) => {
+                  expect(user).toEqual({
+                    userId: setup.stateVariables['userId'],
+                  });
+                },
+              'a (200) response with body an object shaped like {userId: {{userId}}}':
+                (user, setup) => {
+                  expect(user).toEqual({
+                    userId: setup.stateVariables['userId'],
+                  });
+                },
+            },
+            testErrorResponses: {
+              'returns a (404) response without a body': (e) => {
+                expect(e).toBeInstanceOf(UserNotFoundConsumerError);
+              },
+              'a (404) response without a body': (e) => {
+                expect(e).toBeInstanceOf(UserNotFoundConsumerError);
+              },
             },
           },
         },
-        'an http "GET" request to "/users/123" without a body': {
-          trigger: (setup: HttpRequestConfig) =>
-            api(setup.mock['baseUrl']).getUserByPath('123'),
-          testErrorResponses: {
-            'returns a (404) response without a body': (e) => {
-              expect(e).toBeInstanceOf(UserNotFoundConsumerError);
-            },
-            'a (404) response without a body': (e) => {
-              expect(e).toBeInstanceOf(UserNotFoundConsumerError);
-            },
-          },
-        },
-        'an http "GET" request to "/users/{{userId}}" without a body': {
-          trigger: (setup: HttpRequestConfig) =>
-            api(setup.mock['baseUrl']).getUserByPath(
-              setup.stateVariables['userId'] as string,
-            ),
-          testResponses: {
-            'returns a (200) response with body an object shaped like {userId: {{userId}}}':
-              (user, setup) => {
-                expect(user).toEqual({
-                  userId: setup.stateVariables['userId'],
-                });
-              },
-            'a (200) response with body an object shaped like {userId: {{userId}}}':
-              (user, setup) => {
-                expect(user).toEqual({
-                  userId: setup.stateVariables['userId'],
-                });
-              },
-          },
-          testErrorResponses: {
-            'returns a (404) response without a body': (e) => {
-              expect(e).toBeInstanceOf(UserNotFoundConsumerError);
-            },
-            'a (404) response without a body': (e) => {
-              expect(e).toBeInstanceOf(UserNotFoundConsumerError);
-            },
-          },
-        },
-      },
-    });
+      })
+      .forEach((verification) => {
+        // eslint-disable-next-line jest/expect-expect
+        it(`${verification.testName}`, () =>
+          verifier.runPreparedTest(verification));
+      });
   },
 );
