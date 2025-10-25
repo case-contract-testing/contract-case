a577a3f76041d985a818b62e37079d15fb7c41fa
Timothy Jones <timothy.l.jones@gmail.com>
2025-10-25 18:07:55 +1100

    feat(jest): Allow specifying a custom jest timeout, instead of always overriding to 30s

--- packages/contract-case-jest/src/boundaries/jest/jest.ts
+++ packages/contract-case-jest/src/boundaries/jest/jest.ts
@@ -10,21 +10,30 @@ import type {
   VerifyCaseJestCallback,
 } from './types.js';
 
+/**
+ * @internal
+ * The timeout to use to override jest's default timeout.
+ *
+ * If you update this, make sure you update the comments in {@link defineContract} and {@link verifyContract}.
+ */
 const TIMEOUT = 30000;
+
 /**
  * Convenience wrapper for defining contracts
  *
  * @param config - Configuration for this definition run (may be overridden in individual tests)
- * @param callback - The test definitions
+ * @param setupCallback - The test definitions
  * @param onContractDefineSuccess - An optional callback to call if the contract was written successfully
+ * @param timeoutMillis - An optional timeout to set for jest's hooks and tests. Defaults to 30000
  * @returns
  */
 export const defineContract = (
   config: ContractCaseJestConfig,
-  callback: DefineCaseJestCallback,
+  setupCallback: DefineCaseJestCallback,
   onContractDefineSuccess: (
     details: ContractWriteSuccess,
   ) => Promise<void> | void = () => {},
+  timeoutMillis: number = TIMEOUT,
 ): void =>
   describe(`Case contract definition`, () => {
     const contract = new ContractCaseDefiner({
@@ -33,25 +42,42 @@ export const defineContract = (
       ...config,
     });
 
-    afterAll(() => contract.endRecord().then(onContractDefineSuccess), TIMEOUT);
+    afterAll(
+      () => contract.endRecord().then(onContractDefineSuccess),
+      timeoutMillis,
+    );
 
     describe(`between ${config.consumerName} and ${config.providerName}`, () => {
-      jest.setTimeout(TIMEOUT);
+      jest.setTimeout(timeoutMillis);
 
-      callback(contract);
+      setupCallback(contract);
     });
   });
 
+/**
+ * Convenience wrapper for verifying contracts. Calling this will generate all the tests
+ * you need for
+ *
+ * @param config - Configuration for this verification run
+ * @param setupCallback - Optional. Called with the contract verifier before any tests are run. You can use this to do any
+ * computed setup necessary
+ * @param verificationCompleteCallback - Optional. Called once the verification is complete. In a future release, this will contain the details
+ * of the verification success / failure.
+ * @param timeoutMillis - An optional timeout to set for jest's hooks and tests. Defaults to 30000
+ * @returns
+ */
 export const verifyContract = (
   config: ContractCaseJestVerifierConfig,
   setupCallback: VerifyCaseJestCallback = () => {},
+  verificationComplete = () => {},
+  timeoutMillis: number = TIMEOUT,
 ): void => {
   if (!config.providerName) {
     throw new Error('Must specify a providerName to verify');
   }
   describe(`Provider verification for ${config.providerName}`, () => {
     const verifier = new ContractVerifier(config);
-    jest.setTimeout(TIMEOUT);
+    jest.setTimeout(timeoutMillis);
 
     setupCallback(verifier);
 
@@ -63,6 +89,14 @@ export const verifyContract = (
     });
     // TODO: Determine whether Jest runs tests in order always, and if not, do something else here.
     it('Overall verification result', () =>
-      verifier.closePreparedVerification());
+      verifier.closePreparedVerification().then(
+        () => {
+          verificationComplete();
+        },
+        (e) => {
+          verificationComplete();
+          throw e;
+        },
+      ));
   });
 };
