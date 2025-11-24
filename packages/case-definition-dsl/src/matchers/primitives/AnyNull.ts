913232e34e2d75c22c3b657fa6058d0c5286450e
Timothy Jones <timothy.l.jones@gmail.com>
2025-11-24 20:40:15 +1000

    chore: Correct documenation

--- packages/case-definition-dsl/src/matchers/primitives/AnyNull.ts
+++ packages/case-definition-dsl/src/matchers/primitives/AnyNull.ts
@@ -22,8 +22,6 @@ export class AnyNull extends AnyMatcherWithExample {
   /**
    * Matches `null` (useful for languages like Java where `body: null` means no
    * body, but you want to match a eg a json document that has the body `"null"`).
-   *
-   * @param example - Any floating point number, not infinity, not NaN.
    */
   constructor() {
     super(NULL_MATCHER_TYPE, null);
