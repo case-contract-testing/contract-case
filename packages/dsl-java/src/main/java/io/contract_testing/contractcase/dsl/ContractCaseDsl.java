9eb003e74d9a49bf1a4d9b580607beef26d3fdf6
Timothy Jones <timothy.l.jones@gmail.com>
2025-12-15 19:33:52 +1100

    fix(java-dsl): Retain ContractCaseDsl annotation at runtime

--- packages/dsl-java/src/main/java/io/contract_testing/contractcase/dsl/ContractCaseDsl.java
+++ packages/dsl-java/src/main/java/io/contract_testing/contractcase/dsl/ContractCaseDsl.java
@@ -1,10 +1,14 @@
 package io.contract_testing.contractcase.dsl;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Marker interface to mark generated DSL classes as belonging to ContractCase.
  * <p>
  * Any custom serialisers can use this to detect the ContractCase DSL if needed.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ContractCaseDsl {
 
 }
