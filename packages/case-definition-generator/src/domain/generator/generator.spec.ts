3a91186171f6923f7d3a37171f6942b25fa64825
Timothy Jones <timothy.l.jones@gmail.com>
2025-12-22 16:46:33 +1300

    test: Move snapshot to a proper jest snapshot

--- packages/case-definition-generator/src/domain/generator/generator.spec.ts
+++ packages/case-definition-generator/src/domain/generator/generator.spec.ts
@@ -42,29 +42,7 @@ const matcherContainsDefinition: InternalObjectDeclaration = {
 describe('generator', () => {
   describe('typescript', () => {
     it('generates a matcher as expected', () => {
-      expect(generateDslCode(matcherDefinition, '_case'))
-        .toBe(`import { AnyCaseMatcherOrData } from "@contract-case/case-plugin-dsl-types";
-// Constant
-export const ARRAY_EACH_ENTRY_LIKE_TYPE = "_case:ArrayEachEntryLike" as const;
-// Interface
-export interface CoreArrayEachEntryMatchesMatches {
-    "_case:matcher:type": typeof ARRAY_EACH_ENTRY_LIKE_TYPE;
-    "_case:matcher:matcher": AnyCaseMatcherOrData;
-    "_case:matcher:example"?: AnyData[];
-}
-// Factory Function
-/**
- * Matches an array where each element matches the provided matcher.
- *
- * @param matcher - The matcher to match against each element of the array.
- * @param example - Example data to use instead of the generated one
- */
-export const arrayEachEntryMatches = (matcher: AnyCaseMatcherOrData, example?: AnyData[]): CoreArrayEachEntryMatchesMatches => ({
-    "_case:matcher:type": ARRAY_EACH_ENTRY_LIKE_TYPE,
-    "_case:matcher:matcher": matcher,
-    ...(example !== undefined ? { "_case:matcher:example": example } : {})
-});
-`);
+      expect(generateDslCode(matcherDefinition, '_case')).toMatchSnapshot();
     });
   });
   describe('java', () => {
